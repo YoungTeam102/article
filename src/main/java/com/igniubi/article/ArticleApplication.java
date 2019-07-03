7b8a042e02cb7327fc422c67580e540d35ce0291
shuai.zhang <shuai.zhang@yoho.cn>
2019-07-03 16:02:22 +0800

    update

--- src/main/java/com/igniubi/article/ArticleApplication.java
+++ src/main/java/com/igniubi/article/ArticleApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = "com.igniubi")
+@EnableAspectJAutoProxy
 public class ArticleApplication {
 
     public static void main(String[] args) {
